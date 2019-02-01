ef3155bdecfbb8ae2c1d7f6997083322be031b70
Tyler Rathod <trathod@calpoly.edu>
2019-02-01 15:53:45 -0800

    Add config options in main.js to filter what slides are pulled in based on quarter/year

--- js/main.js
+++ js/main.js
@@ -5,13 +5,22 @@ const client = contentful.createClient({
     accessToken: "9ea93dc1aafc73f61ba2e9d30407d24a1030d5579108d4164550f53d1f611a6d"
   });
   var data = [];
+  // SET THIS VALUE TO DESIRED QUARTER / YEAR
+  var year = 2019;
+  var quarter = "Fall"
+  
   // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
   client
-    .getEntry({
+    .getEntries({
         content_type: 'slide'
     })
     .then(function(content) {
-        data.push(content);
-        console.log(content);
+        for (i in content.items) {
+            if (content.items[i].fields.quarter == quarter && content.items[i].fields.year == year) {
+                console.log("match");
+                data.push(content.items[i]);
+            };
+        };
+        // data.push(content);
     })
     .catch(err => console.log(err));
